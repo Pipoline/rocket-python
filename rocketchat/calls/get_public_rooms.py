b4d9ddef596cc8ab4729fef7a889d79e827770a7
Derek Stegelman <dstegelman@gmail.com>
2016-07-09 07:29:03 -0600

    Add get_public_rooms

---
+++ rocketchat/calls/get_public_rooms.py
@@ -0,0 +1,31 @@
+import logging
+
+from rocketchat.calls.base import RocketChatBase
+
+logger = logging.getLogger(__name__)
+
+
+class GetPublicRooms(RocketChatBase):
+    endpoint = '/api/publicRooms'
+
+    def build_endpoint(self):
+        return self.endpoint
+
+    def post_response(self, result):
+        rooms = []
+        
+        try:
+            _rooms = result.get('rooms')
+
+            for room in _rooms:
+                room_dict = {}
+                room_dict['name'] = room.get('name')
+                room_dict['id'] = room.get('_id')
+                rooms.append(room_dict)
+
+        except Exception as e:
+            logger.error('Exception in fetching public rooms {e}'.format(
+                e=e
+            ), exc_info=True)
+
+        return rooms
