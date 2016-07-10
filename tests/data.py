d815fd217177a00d0e806a247ed7897b7cde9d1a
Derek Stegelman <dstegelman@gmail.com>
2016-07-09 18:37:00 -0600

    tests for public rooms

---
+++ tests/data.py
@@ -0,0 +1,4 @@
+PUBLIC_ROOM_TEST = {'rooms': [
+    {'name': 'Test Room',
+     '_id': '123456'}
+]}
--- tests/test_api.py
+++ tests/test_api.py
@@ -5,13 +5,19 @@ from rocketchat.api import RocketChatAPI
 from rocketchat.calls.send_message import SendMessage
 
 
-class APITestCase(unittest.TestCase):
+from .data import PUBLIC_ROOM_TEST
+
+
+class APITestCase(object):
 
     def setUp(self):
         self.settings = {'username': 'something',
                          'password': 'something',
                          'domain': 'https://www.example.com'}
         self.api = RocketChatAPI(settings=self.settings)
+
+
+class SendMessageTestCase(APITestCase, unittest.TestCase):
 
     @mock.patch('rocketchat.calls.base.RocketChatBase.set_auth_token')
     @mock.patch('rocketchat.calls.base.RocketChatBase.set_auth_headers')
@@ -36,3 +42,22 @@ class APITestCase(unittest.TestCase):
         request_mock.return_value = mock_response
 
         self.api.send_message('12312', 'Test message')
+
+
+class GetPublicRoomTestCase(APITestCase, unittest.TestCase):
+
+    @mock.patch('rocketchat.calls.base.RocketChatBase.set_auth_token')
+    @mock.patch('rocketchat.calls.base.RocketChatBase.set_auth_headers')
+    @mock.patch('requests.Session.request')
+    def test_get_public_rooms(self, mock_request, set_auth_headers_mock, set_auth_mock):
+        set_auth_mock.return_value = None
+        set_auth_headers_mock.return_value = None
+
+        mock_response = mock.Mock()
+        mock_response.json.return_value = PUBLIC_ROOM_TEST
+
+        mock_request.return_value = mock_response
+
+        pub_rooms = self.api.get_public_rooms()
+        self.assertEqual(pub_rooms[0]['name'], 'Test Room')
+        self.assertEqual(pub_rooms[0]['id'], '123456')
