665b5a4070172788fd90107a5ac70c12f6dfd551
Derek Stegelman <dstegelman@gmail.com>
2017-01-21 20:19:34 -0700

    Bump 1.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='rocket-python',
-      version='1.1.0',
+      version='1.2.0',
       description="RocketChat API with Python",
       long_description=open("README.md", "r").read(),
       classifiers=[
