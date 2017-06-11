983c9e8308d3811c9280cebb7ec0d7e57bb0aa6f
Hendrik <e.Hendrik@gmail.com>
2017-06-12 00:49:59 +0200

    removed a print statement

--- rocketchat/calls/channels/get_history.py
+++ rocketchat/calls/channels/get_history.py
@@ -6,33 +6,8 @@ logger = logging.getLogger(__name__)
 
 
 class GetRoomHistory(RocketChatBase):
-    endpoint = '/api/v1/channels.history'
-
-    '''def build_endpoint(self, **kwargs):
-        if 'oldest' in kwargs:
-            return '{endpoint}?roomId={room_id}&oldest={oldest}'.format(
-                endpoint=self.endpoint,
-                oldest=kwargs.get('oldest'),
-                room_id=kwargs.get('room_id')
-            )
-        else:
-            return '{endpoint}?roomId={room_id}'.format(
-                endpoint=self.endpoint,
-                room_id=kwargs.get('room_id')
-            )'''
+    endpoint = '/api/v1/channels.history'          
     def build_endpoint (self, **kwargs):
-        for x in kwargs:
-            print("{} ist: {}".format(x,kwargs.get(x)))
-        print('{endpoint}?roomId={room_id}&oldest={oldest}&latest={latest}&inclusive={inclusive}&count={count}&unreads={unreads}'.format(
-                endpoint=self.endpoint,
-                oldest=kwargs.get('oldest'),
-                room_id=kwargs.get('room_id'),
-                latest=kwargs.get('latest'),
-                inclusive=str(kwargs.get('inclusive')).lower(),
-                count=kwargs.get('count'),
-                unreads=str(kwargs.get('unreads')).lower()
-                ))
-        #latest=None,inclusive=None, count=None,unreads=None,
         return '{endpoint}?roomId={room_id}&oldest={oldest}&inclusive={inclusive}&count={count}&unreads={unreads}&latest={latest}'.format(
                 endpoint=self.endpoint,
                 oldest=kwargs.get('oldest'),
