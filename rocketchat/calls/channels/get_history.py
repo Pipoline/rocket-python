4562daef5d673831165b0f280dca465dadc1288e
Hendrik <e.Hendrik@gmail.com>
2017-06-12 00:40:20 +0200

    Add options
    Added all options to the public get room history function

--- rocketchat/calls/channels/get_history.py
+++ rocketchat/calls/channels/get_history.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 class GetRoomHistory(RocketChatBase):
     endpoint = '/api/v1/channels.history'
 
-    def build_endpoint(self, **kwargs):
+    '''def build_endpoint(self, **kwargs):
         if 'oldest' in kwargs:
             return '{endpoint}?roomId={room_id}&oldest={oldest}'.format(
                 endpoint=self.endpoint,
@@ -19,7 +19,29 @@ class GetRoomHistory(RocketChatBase):
             return '{endpoint}?roomId={room_id}'.format(
                 endpoint=self.endpoint,
                 room_id=kwargs.get('room_id')
-            )
+            )'''
+    def build_endpoint (self, **kwargs):
+        for x in kwargs:
+            print("{} ist: {}".format(x,kwargs.get(x)))
+        print('{endpoint}?roomId={room_id}&oldest={oldest}&latest={latest}&inclusive={inclusive}&count={count}&unreads={unreads}'.format(
+                endpoint=self.endpoint,
+                oldest=kwargs.get('oldest'),
+                room_id=kwargs.get('room_id'),
+                latest=kwargs.get('latest'),
+                inclusive=str(kwargs.get('inclusive')).lower(),
+                count=kwargs.get('count'),
+                unreads=str(kwargs.get('unreads')).lower()
+                ))
+        #latest=None,inclusive=None, count=None,unreads=None,
+        return '{endpoint}?roomId={room_id}&oldest={oldest}&inclusive={inclusive}&count={count}&unreads={unreads}&latest={latest}'.format(
+                endpoint=self.endpoint,
+                oldest=kwargs.get('oldest'),
+                room_id=kwargs.get('room_id'),
+                inclusive=kwargs.get('inclusive'),
+                count=kwargs.get('count'),
+                unreads=kwargs.get('unreads'),
+                latest=kwargs.get('latest')
+                )
 
     def post_response(self, result):
         return result
