112006c636f9e51f5c9d258519822b76d6f02ce4
Hendrik <e.Hendrik@gmail.com>
2017-06-12 21:03:48 +0200

    Add files via upload

--- rocketchat/calls/channels/get_history.py
+++ rocketchat/calls/channels/get_history.py
@@ -6,17 +6,18 @@ logger = logging.getLogger(__name__)
 
 
 class GetRoomHistory(RocketChatBase):
-    endpoint = '/api/v1/channels.history'          
-    def build_endpoint (self, **kwargs):
+    endpoint = '/api/v1/channels.history'
+
+    def build_endpoint(self, **kwargs):
         return '{endpoint}?roomId={room_id}&oldest={oldest}&inclusive={inclusive}'\
                 '&count={count}&unreads={unreads}&latest={latest}'.format(
-                endpoint=self.endpoint,
-                oldest=kwargs.get('oldest'),
-                room_id=kwargs.get('room_id'),
-                inclusive=kwargs.get('inclusive'),
-                count=kwargs.get('count'),
-                unreads=kwargs.get('unreads'),
-                latest=kwargs.get('latest')
+                 endpoint=self.endpoint,
+                 oldest=kwargs.get('oldest'),
+                 room_id=kwargs.get('room_id'),
+                 inclusive=kwargs.get('inclusive'),
+                 count=kwargs.get('count'),
+                 unreads=kwargs.get('unreads'),
+                 latest=kwargs.get('latest')
                 )
 
     def post_response(self, result):
