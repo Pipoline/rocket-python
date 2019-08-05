7f90aaa8d8c97b1aa33d999a8292d3355cc58bba
Peter Gonda <peter@pipoline.com>
2019-08-05 10:52:53 +0200

    Bump version: 1.2.8 → 1.2.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='rocket-python',
-      version='1.2.8',
+      version='1.2.9',
       description="RocketChat API with Python",
       long_description=open("README.md", "r").read(),
       classifiers=[
