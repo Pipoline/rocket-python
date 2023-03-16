d7daf37b7692937f1ed390c8c6580e814889743a
Thibault Vataire <thibault[at]beezim.fr>
2023-03-16 23:08:27 +0100

    Signature of method 'GetUsers.build_endpoint' does not match signature of the base method in class 'RocketChatBase'.

--- rocketchat/calls/users/get_users.py
+++ rocketchat/calls/users/get_users.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 class GetUsers(RocketChatBase):
     endpoint = '/api/v1/users.list'
 
-    def build_endpoint(self):
+    def build_endpoint(self, **kwargs):
         return self.endpoint
 
     def post_response(self, result):
